faedd711c2c0762cd20fb1dd39fd11cb6ebdf6f2
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-06-29 23:12:21 +0100

    first commit

---
+++ main/static/main/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+    // $('li.active').removeClass('active');
+    // $('a[href="' + location.pathname + '"]').closest('li').addClass('active');
+
+    // $('.add_pho').click(function () {
+    //     $('#add_try').show()
+    //     $('#show_inp').removeClass('active')
+    //     $('#tried').removeClass('active')
+    //     $('#paint_comments').removeClass('active')
+    //     $('#comments').hide()
+    //     $('#tries').hide()
+    //     $('#add_comment').hide()
+    // })
+
+
+    // $('.carousel-item').eq(0).addClass('active')
+    // $('div.carousel-inner > div:first-child').addClass('active')
+
+});
