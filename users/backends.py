50d668c9987a14432f949af5c33bb20358511d1d
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-07-06 22:42:47 +0100

    added custom email authenticated backend

--- users/backends.py
+++ users/backends.py
@@ -1,19 +1,17 @@
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
-from django.db.models import Q
+# from django.db.models import Q
 
 UserModel = get_user_model()
 
-
 class EmailBackend(ModelBackend):
-    def authenticate(self, request, email=None, password=None, **kwargs):
+    def authenticate(self, request, username=None, password=None, **kwargs):
         try:
-            user = UserModel.objects.get(Q(email=email))
-            # user = UserModel.objects.get(Q(username__iexact=username) | Q(email__iexact=username))
+            user = UserModel.objects.get(email=username)
         except UserModel.DoesNotExist:
             UserModel().set_password(password)
         except UserModel.MultipleObjectsReturned:
-            return UserModel.objects.filter(email=email).order_by('id').first()
+            return UserModel.objects.filter(email=username).order_by('id').first()
         else:
             if user.check_password(password) and self.user_can_authenticate(user):
                 return user
