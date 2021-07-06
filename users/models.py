2db5ca02b1605a3bbd150f0bf23dc67fb61a16bc
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-07-06 22:40:33 +0100

    added a custom user model and a base manager

--- users/models.py
+++ users/models.py
@@ -1,18 +1,37 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.core.validators import RegexValidator
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
+class UserManager(BaseUserManager):
+    use_in_migrations = True
 
-alphanumeric = RegexValidator(r'^[0-9a-zA-Z]*$', 'Only alphanumeric characters are allowed.')
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email=email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-# class CustomUser(AbstractUser):
-#     email = models.EmailField(unique=True, max_length=60)
-#     username = models.CharField(max_length=50, unique=True, validators=[alphanumeric])
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
 
-#     USERNAME_FIELD = 'email'
-#     REQUIRED_FIELDS = []
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True')
+        return self._create_user(email, password, **extra_fields)
 
-#     def __str__(self):
-#         return self.username
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True, max_length=60)
+    username = None
+    # username = models.CharField(max_length=50, unique=True, validators=[alphanumeric])
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
+    objects =  UserManager()
+
+    def __str__(self):
+        return self.username if self.username else self.email
