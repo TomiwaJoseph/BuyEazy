72f9175dd18b88eafc8c4f8d46240ce5e0b36fd1
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-07-06 22:43:49 +0100

    added more url paths

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('shop/', views.shop, name='shop'),
+    path('checkout/', views.checkout_view, name='checkout'),
     path('single_product/', views.single_product, name='single_product'),
 ]
