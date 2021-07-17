3ca620d2fb444d2f6856c03bfc311521a7716046
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-07-17 19:02:45 +0100

    added newsletter model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Newsletter(models.Model):
+    email = models.EmailField(blank=False, null=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
