a6caa10ecfef648b7fcc0dab071cc739e3c7309c
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-07-17 19:04:17 +0100

    added more views

--- main/views.py
+++ main/views.py
@@ -1,11 +1,48 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from shop.models import Product, Category
+import random
+from .models import Newsletter
 
-# Create your views here.
+
+# Ajax Requests Start
+
+def newsletter(request):
+    get_email = request.POST.get('email')
+    email_in_newsletter = Newsletter.objects.filter(email=get_email)
+    if not email_in_newsletter:
+        new_email = Newsletter.objects.create(email=get_email)
+        new_email
+
+    return HttpResponse('Success')
+
+# Ajax Requests Ends
+
+
 def index(request):
-    return render(request, 'main/index.html')
+    all_categories = []
+    a = list(Product.objects.all())
+    b = [i.title for i in list(Category.objects.all())]
+    random.shuffle(b)
+    random.shuffle(a)
+
+    for num in range(4):
+        l = [i for i in a if i.category.title == b[num]]
+        all_categories.append(l[0])
+    
+    context = {
+        'carousel': all_categories[:3],
+        'all_categories': all_categories,
+    }
+    return render(request, 'main/index.html', context)
 
 def contact(request):
     return render(request, 'main/contact.html')
 
-def shop(request):
-    return render(request, 'main/shop.html')
+# def newsletter(request):
+#     get_email = request.POST.get('email')
+#     # if get_email:
+#     #     new_email = Newsletter.objects.create(email=get_email)
+#     #     new_email.save()
+#     return HttpResponse(status=204)
+
