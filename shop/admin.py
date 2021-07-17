d7d50b8ca0b2193f39a8af1e85410cf45d0db8f6
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-07-17 19:02:21 +0100

    added models, created and added custom admin views

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import (Product, Reviews, ProductImages, 
+    Category, Wishlist)
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    search_fields = ['title',]
+    list_filter = ['category',]
+    list_editable = ['availability']
+    list_display = ('title', 'category', 'availability', 'image_tag')
+    prepopulated_fields = {'slug': ('title',)}
+
+    def make_available(modeladmin, request, queryset):
+        queryset.update(availability=True)
+    
+    def make_unavailable(modeladmin, request, queryset):
+        queryset.update(availability=False)
+
+    admin.site.add_action(make_available, "Mark as available")
+    admin.site.add_action(make_unavailable, "Mark as unavailable")
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    # list_display = ('')
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(ProductImages)
+admin.site.register(Reviews)
+admin.site.register(Wishlist)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+
