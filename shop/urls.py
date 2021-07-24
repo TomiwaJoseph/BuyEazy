b5185436a722f0807b5d324837bec2c94b467c7f
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-07-24 23:30:13 +0100

    added more paths

--- shop/urls.py
+++ shop/urls.py
@@ -3,13 +3,18 @@ from . import views
 
 urlpatterns = [
     # Ajax Requests
-    path('add_to_wishlist/', views.add_to_wishlist, name='add_to_wishlist'),
-    path('add_to_cart/', views.add_to_cart, name='add_to_cart'),
+    path('add-to-wishlist/', views.add_to_wishlist, name='add_to_wishlist'),
+    path('add-to-cart/', views.add_to_cart, name='add_to_cart'),
+    path('load-more-products/', views.load_more_products, name='load_more_products'),
+    path('delete-from-cart/', views.delete_cart_item, name='delete_cart_item'),
+    path('update-cart/', views.update_cart_item, name='update_cart_item'),
+    path('show-preview/', views.show_cart_preview, name='show_preview'),
     # Ajax Requests Ends
 
     path('test_page/', views.testpage, name='test'),
     path('shop/', views.shop, name='shop'),
-    path('checkout/', views.checkout_view, name='checkout'),
-    # path('view_product/<slug:slug>', views.single_product, name='view_product'),
-    path('view_product/<slug:slug>', views.SingleProduct.as_view(), name='view_product'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.CheckoutView.as_view(), name='checkout'),
+    path('view-product/<slug:slug>', views.SingleProduct.as_view(), name='view_product'),
 ]
--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('login/', views.MyLoginView.as_view(redirect_authenticated_user=True), name='login'),
     path('register/', views.register, name='register'),
     path('logout/', views.logout_view, name='logout'),
