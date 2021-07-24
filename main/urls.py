31e0625c30cde147eeea8a6e14df46c93b78fc94
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-07-24 23:32:13 +0100

    added more paths

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('search/', views.search_product, name='search'),
     path('contact/', views.contact, name='contact'),
     path('newsletter/', views.newsletter, name='newsletter'),
 ]
