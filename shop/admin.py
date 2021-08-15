e7d98dfb440ed0f4589bc70c42f9af12d9066a74
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-08-15 22:24:56 +0100

    added more customization to OrderItem, Order, Address models

--- shop/admin.py
+++ shop/admin.py
@@ -1,23 +1,28 @@
 from django.contrib import admin
-from .models import (Product, Reviews, ProductImages, 
-    Category, Wishlist)
+from .models import (Product, Reviews, ProductImages, Refund,
+    Category, OrderItem, Order, Payment, Coupon, Address)
 
 
 class ProductAdmin(admin.ModelAdmin):
     search_fields = ['title',]
     list_filter = ['category',]
-    list_editable = ['availability', 'category', 'price', 'discount_price']
-    list_display = ('title', 'price', 'discount_price', 'category', 'availability', 'image_tag')
+    list_editable = ['category', 'price', 'discount_price']
+    list_display = ('title', 'price', 'discount_price', 'category', 'image_tag')
     prepopulated_fields = {'slug': ('title',)}
 
-    def make_available(modeladmin, request, queryset):
-        queryset.update(availability=True)
-    
-    def make_unavailable(modeladmin, request, queryset):
-        queryset.update(availability=False)
 
-    admin.site.add_action(make_available, "Mark as available")
-    admin.site.add_action(make_unavailable, "Mark as unavailable")
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['user', 'product', 'quantity']
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_editable = ['being_delivered', 'received', 'refund_requested', 'refund_granted']
+    list_display = ['user', 'ordered', 'being_delivered', 'received',
+        'refund_requested', 'refund_granted', 'coupon']
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['user', 'street_address', 'country', 'address_type']
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -25,8 +30,12 @@ class CategoryAdmin(admin.ModelAdmin):
 
 
 admin.site.register(ProductImages)
+admin.site.register(Address, AddressAdmin)
+admin.site.register(Coupon)
+admin.site.register(Payment)
 admin.site.register(Reviews)
-admin.site.register(Wishlist)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem, OrderItemAdmin)
 
