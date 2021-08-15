141e06d60bbddecfd30039f6dc8a9aa71d84621f
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-08-15 22:30:51 +0100

    import Wishlist model and added it to context data

--- users/views.py
+++ users/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import views as auth_views
 from django.contrib.auth import logout
 from .forms import (LoginForm, RegisterForm)
+from shop.models import OrderItem, Order
+from .models import Wishlist
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -23,8 +26,17 @@ def register(request):
     return render(request, 'registration/register.html', {'form': form})
 
 def logout_view(request):
+    # all_orders = Order.objects.filter(user=request.user, ordered=False)
+    # all_orders.delete()
+    # all_order_items = OrderItem.objects.filter(user=request.user, ordered=False)
+    # all_order_items.delete()
     logout(request)
     return redirect('index')
 
+@login_required
 def dashboard(request):
-    return render(request, 'users/dashboard.html')
+    all_wishlist = Wishlist.objects.filter(user=request.user)
+    context = {
+        'all_wishlist': all_wishlist,
+    }
+    return render(request, 'users/dashboard.html', context)
