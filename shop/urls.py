d056f47a5d148ea596ab60138d53a231009a8039
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-08-23 23:01:14 +0100

    added more routes

--- shop/urls.py
+++ shop/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     # Ajax Requests
+    path('specific-category/', views.filter_category, name='filter_category'),
+    path('specific-category-and-price/', views.filter_price_and_product, name='filter_price_and_product'),
     path('add-to-wishlist/', views.add_to_wishlist, name='add_to_wishlist'),
     path('add-to-cart/', views.add_to_cart, name='add_to_cart'),
     path('load-more-products/', views.load_more_products, name='load_more_products'),
@@ -11,12 +13,17 @@ urlpatterns = [
     path('show-preview/', views.show_cart_preview, name='show_preview'),
     # Ajax Requests Ends
 
-    path('create-payment-intent/', views.StripeIntentView.as_view(), name="create_payment_intent"),
-    path('create-checkout-session/', views.CreateCheckoutSessionView.as_view(), name="create_checkout"),
-    path('landing-view/', views.StripeLandingView.as_view(), name="stripe_landing"),
+    # Stripe
+    path('landing/', views.TestView.as_view(), name='landing'),
     path('success/', views.SuccessView.as_view(), name="success"),
     path('cancel/', views.CancelView.as_view(), name="cancel"),
-    path('webhoks/stripe/', views.stripe_webhook, name="stripe_webhook"),
+    
+    #To use for project
+    path('create-payment-intent/', views.StripeIntentView.as_view(), name="create_payment_intent"),
+    path('webhooks/stripe/', views.stripe_webhook, name="stripe_webhook"),
+    path('payment/stripe/', views.StripeLandingView.as_view(), name="stripe_payment"),
+    path('payment/paypal/', views.PaypalPaymentView.as_view(), name='paypal_payment'),
+    # End Stripe
     
     
     path('test_page/', views.testpage, name='test'),
@@ -26,7 +33,6 @@ urlpatterns = [
     path('cart/', views.cart, name='cart'),
     path('request-refund/', views.RequestRefundView.as_view(), name='request_refund'),
     path('add-coupon/', views.AddCouponView.as_view(), name='add_coupon'),
-    path('payment/<payment_option>/', views.PaymentView.as_view(), name='payment'),
     path('checkout/', views.CheckoutView.as_view(), name='checkout'),
     path('view-product/<slug:slug>', views.SingleProduct.as_view(), name='view_product'),
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,32 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    # Ajax Requests
+    path('delete-wishlist-item/', views.delete_wishlist_item, name='delete_wishlist_item'),
+    path('show-order-details/', views.show_order_details, name='show_order_details'),
+
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name="registration/password_reset.html"), 
+        name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name="registration/password_reset_done.html"), 
+        name='password_reset_done'),
+    path('password-reset-confim/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name="registration/password_reset_confirm.html"), 
+        name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name="registration/password_reset_complete.html"), 
+        name='password_reset_complete'),
+
     path('dashboard/', views.dashboard, name='dashboard'),
-    path('login/', views.MyLoginView.as_view(redirect_authenticated_user=True), name='login'),
+    path('login/', views.MyLoginView.as_view(
+        redirect_authenticated_user=True), name='login'),
+    # path('login/', auth_views.LoginView.as_view(
+    #     redirect_authenticated_user=True), name='login'),
     path('register/', views.register, name='register'),
-    path('logout/', views.logout_view, name='logout'),
+    path('awaiting-activation/', views.awaiting_activation, name='awaiting_activation'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
 ]
