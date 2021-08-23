f66510ffc132109d512d29b741f82aa7f88a7fc3
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-08-23 23:03:26 +0100

    added event listeners to dashboard's buttons

--- main/static/main/js/main.js
+++ main/static/main/js/main.js
@@ -6,25 +6,11 @@ $(document).ready(function () {
         document.getElementById("search_text").focus();
     });
 
-    $('#all_cat').addClass('active');
+    // $('#all_cat').addClass('active');
 
     $(".carousel-item").eq(0).addClass('active');
     // $('div.carousel-inner > div:first-child').addClass('active')
 
-    const filter = document.querySelector('#sidebar_filt_but');
-    // all_filter_buttons = filter.querySelectorAll('button');
-    all_filter_buttons = $('#sidebar_filt_but').find('button')
-    totalButtons = all_filter_buttons.length;
-
-    for (let i = 0; i < totalButtons; i++) {
-        const button = all_filter_buttons[i]
-        button.addEventListener('click', function () {
-            for (let j = 0; j < totalButtons; j++) {
-                all_filter_buttons[j].classList.remove('active');
-            }
-            this.classList.add('active');
-        })
-    };
 
     // $('li.active').removeClass('active');
     // $('a[href="' + location.pathname + '"]').closest('li').addClass('active');
@@ -34,19 +20,6 @@ $(document).ready(function () {
     setTimeout(function () {
         $('body').find('.cart').fadeIn('slow');
     }, 1000);
-
-    // $('#show_cart').click(function(e){
-    //     e.preventDefault();
-    //     know_state = $('.cart').attr("data-toggle")
-    //     if (know_state == 'inactive'){
-    //         $('body').addClass('modal-open');
-    //         $('body').find('.cart').attr('data-toggle', 'active');
-    //     }
-    //     else {
-    //         $('body').removeClass('modal-open');
-    //         $('body').find('.cart').attr('data-toggle', 'inactive');    
-    //     };
-    // });
 
     $('.cart').on('click', '.label', function () {
         $('body').addClass('modal-open');
@@ -59,12 +32,13 @@ $(document).ready(function () {
     });
     //Toggle
 
-    // $('#wishlist_section').hide()
-
-    $('#wish_sidebar_btn').addClass('active')
-    $('#wishlist_section').show();
+    
+    $('#dash_btn').addClass('active')
+    $('#dashboard_section').show();
+    $('#wishlist_section').hide();
     $('#orders_section').hide();
     $('#password_change_section').hide();
+    $('#your_addresses_section').hide();
 
 
     const sidebar_filter = $('#dashboard_sidebar');
@@ -74,21 +48,38 @@ $(document).ready(function () {
     for (let i = 0; i < sidebarButtonCount; i++){
         const side_buttons = all_sidebar_buttons[i];
         side_buttons.addEventListener('click', function(){
-            // console.log(side_buttons.innerHTML);
             for (let j = 0; j < sidebarButtonCount; j++){
                 all_sidebar_buttons[j].classList.remove('active');
             }
             this.classList.add('active');
             if (this.innerHTML == 'Wishlist'){
+                $('#dashboard_section').hide();
                 $('#wishlist_section').show();
                 $('#orders_section').hide();
                 $('#password_change_section').hide();
+                $('#your_addresses_section').hide();
             } else if (this.innerHTML == 'Orders'){
+                $('#dashboard_section').hide();
                 $('#orders_section').show();
                 $('#wishlist_section').hide();
                 $('#password_change_section').hide();
-            } else {
+                $('#your_addresses_section').hide();
+            } else if (this.innerHTML == 'Dashboard'){
+                $('#dashboard_section').show();
+                $('#orders_section').hide();
+                $('#wishlist_section').hide();
+                $('#password_change_section').hide();
+                $('#your_addresses_section').hide();
+            } else if (this.innerHTML == 'Profile edit'){
+                $('#dashboard_section').hide();
                 $('#password_change_section').show();
+                $('#orders_section').hide();
+                $('#wishlist_section').hide();
+                $('#your_addresses_section').hide();
+            } else {
+                $('#dashboard_section').hide();
+                $('#your_addresses_section').show();
+                $('#password_change_section').hide();
                 $('#orders_section').hide();
                 $('#wishlist_section').hide();
             }
