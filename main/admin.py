317dbf610154599ef522380fdebb803aa9df0658
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-09-22 12:32:50 +0100

    added gallery to admin

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Newsletter
+from .models import Newsletter, Gallery
 
 # Register your models here.
 admin.site.register(Newsletter)
+admin.site.register(Gallery)
