077e20ef8e68f8f152e5d47a22960fef54d3ef7c
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-11-27 02:25:06 +0100

    added all things stripe

--- shop/views.py
+++ shop/views.py
@@ -439,7 +439,10 @@ class CheckoutView(LoginRequiredMixin, View):
 
             return redirect('payment_page')
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 7f5f582f13a2502ba1afead43372a85f8801b869
 @login_required
 def payment_page(request):    
     cart_contents = OrderItem.objects.filter(
@@ -453,6 +456,26 @@ def payment_page(request):
     }
     return render(request, "shop/payment_page.html", context)
 
+<<<<<<< HEAD
+=======
+order = OrderItem.objects.filter(
+            user__email="tomjoe@gmail.com", ordered=False,
+        )
+line_items = [{
+                'price_data': {
+                    'currency': 'usd',
+                    'unit_amount': item.get_stripe_price(),
+                    'product_data': {
+                        'name': item.product.title,
+                        # 'images': [item.product.main_image.path],
+                    },
+                },
+                'quantity': item.quantity,
+            } for item in order]
+print(line_items)
+print()
+
+>>>>>>> 7f5f582f13a2502ba1afead43372a85f8801b869
 
 class CreateCheckoutSessionView(View):
     def post(self, request, *args, **kwargs):
@@ -484,6 +507,7 @@ class CreateCheckoutSessionView(View):
         return JsonResponse({
             'id': checkout_session.id
         })
+<<<<<<< HEAD
 
 
 @csrf_exempt
@@ -542,10 +566,61 @@ def stripe_webhook(request):
 
     return HttpResponse(status=200)
 
+=======
+>>>>>>> 7f5f582f13a2502ba1afead43372a85f8801b869
 
 class SuccessView(TemplateView):
     template_name = "shop/success.html"
 
+<<<<<<< HEAD
+=======
+@csrf_exempt
+def stripe_webhook(request):
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
+        )
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return HttpResponse(status=400)
+
+    # Handle the checkout.session.completed event
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+        
+        print(session)
+        
+        customer_email = session["customer_details"]["email"]
+        course_id = session["metadata"]["products"]
+        email = session["metadata"]["current_user_email"]
+
+        # course = Product.objects.get(id=course_id)
+        # user = Product.objects.get(user__email=email)
+                
+        # course.students.add(user.user)
+        # user.courses_bought.add(course)
+
+        # send_mail(
+        #     subject="Here is your course",
+        #     message=f'Thanks for purchasing the course "{course.title}".\n Enjoy!',
+        #     recipient_list=[customer_email],
+        #     from_email=settings.EMAIL_HOST_USER
+        # )
+
+    return HttpResponse(status=200)
+
+
+class SuccessView(TemplateView):
+    template_name = "shop/success.html"
+
+>>>>>>> 7f5f582f13a2502ba1afead43372a85f8801b869
 
 class CancelView(TemplateView):
     template_name = "shop/cancel.html"
