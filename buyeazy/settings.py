b64df34c9b23e9e2372424abae47cd3b88d92d14
TomiwaJoseph <tomiwajoseph88@gmail.com>
2023-02-03 14:46:07 +0100

    deployment changes

--- buyeazy/settings.py
+++ buyeazy/settings.py
@@ -71,16 +71,16 @@ WSGI_APPLICATION = 'buyeazy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.parse(config('DATABASE_URL'), conn_max_age=600),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.parse(config('DATABASE_URL'), conn_max_age=600),
+# }
 
 
 # Password validation
