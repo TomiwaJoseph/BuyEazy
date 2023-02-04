5bf0c13a1c58848949a3b8c7dca1d9cdf13c04a2
TomiwaJoseph <tomiwajoseph88@gmail.com>
2023-02-04 16:52:03 +0100

    removed dj-database-url from imports

--- buyeazy/settings.py
+++ buyeazy/settings.py
@@ -1,6 +1,5 @@
 from decouple import config
 from pathlib import Path
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
